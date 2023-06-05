474205ac01c05a4f602f4d7275870ab9111f762f
Rekzar <a.alcala@alumnos.upm.es>
2023-06-05 09:31:06 +0200

    Intento fallido de busqueda
    No entra el addOnSuccessListener

--- app/src/main/java/com/example/tfgym/principal/ui/crearRutinas/ejercicios/EjerciciosScreen.kt
+++ app/src/main/java/com/example/tfgym/principal/ui/crearRutinas/ejercicios/EjerciciosScreen.kt
@@ -1,23 +1,25 @@
 package com.example.tfgym.principal.ui.crearRutinas.ejercicios
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.graphics.Color.Companion.Gray
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 @Composable
 fun EjerciciosScreen(){
@@ -27,38 +29,55 @@ fun EjerciciosScreen(){
     // Obtener la lista de ejercicios basada en el texto de búsqueda
     val ejercicios = remember { mutableStateListOf<Ejercicio>() }
 
-    // Lógica para obtener los ejercicios según el texto de búsqueda
-    obtenerEjercicios(searchTextState.value, ejercicios)
+    // Obtener la lista de ejercicios basada en el texto de búsqueda
+    val resultados = remember { mutableStateListOf<Ejercicio>() }
+
+
 
     Column {
         // Barra de búsqueda
-        TextField(
-            value = searchTextState.value,
-            onValueChange = { searchTextState.value = it },
-            label = { Text("Buscar ejercicio")},
-            modifier = Modifier.fillMaxWidth()
-        )
-        if (searchTextState.value.isNotEmpty()) {
-            DropdownMenu(
-                expanded = true,
-                onDismissRequest = { searchTextState.value = "" }
-            ){
-                ejercicios.forEach { ejercicio ->
-                    DropdownMenuItem(onClick = { ejercicios.add(ejercicio) }) {
-                        Text(ejercicio.name)
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            TextField(
+                value = searchTextState.value,
+                onValueChange = {
+                    searchTextState.value = it
+                },
+                label = { Text("Buscar ejercicio") },
+            )
+
+            IconButton(
+                onClick = {
+                    obtenerEjercicios(searchTextState.value, resultados)
+                }
+            ) {
+                Icon(
+                    Icons.Filled.Search,
+                    contentDescription = "Buscar",
+                    tint = Gray
+                )
+            }
+        }
+
+            if (resultados.isNotEmpty()) {
+                LazyColumn(modifier = Modifier.weight(1f)) {
+                    items(resultados) { ejercicio ->
+                        EjercicioItem(ejercicio)
+                    }
+                }
+            } else {
+                // Mostrar la lista original
+                LazyColumn(modifier = Modifier.weight(1f)) {
+                    items(ejercicios) { ejercicio ->
+                        EjercicioItem(ejercicio)
                     }
                 }
             }
         }
-
-        // Lista de resultados
-        LazyColumn {
-            items(ejercicios) { ejercicio ->
-                EjercicioItem(ejercicio)
-            }
-        }
     }
-}
 
 @Preview
 @Composable
@@ -73,25 +92,25 @@ fun EjercicioItem(ejercicio: Ejercicio) {
 }
 
 fun obtenerEjercicios(searchText: String, ejercicios: SnapshotStateList<Ejercicio>) {
-    val ejercicios = mutableListOf<Ejercicio>()
 
-    val db = FirebaseFirestore.getInstance()
-    val ejerciciosCollection = db.collection("ejercicios")
+    val db = Firebase.firestore
+    val ejerciciosCollection = db.collection("Ejercicios")
 
-    //Búsqueda dinámica de ejercicios
-    ejerciciosCollection.whereGreaterThanOrEqualTo("name", searchText)
-        .whereLessThanOrEqualTo("name", searchText + "\uf8ff")
+
+    //Búsqueda de ejercicios
+    ejerciciosCollection
         .get()
         .addOnSuccessListener { querySnapshot ->
             for (document in querySnapshot.documents) {
                 val ejercicio = document.toObject(Ejercicio::class.java)
-                ejercicio?.let {
-                    ejercicios.add(it)
+                if (ejercicio?.name == searchText){
+                    ejercicio?.let {
+                        ejercicios.add(it)
+                    }
                 }
             }
         }
         .addOnFailureListener { exception ->
-            // Ocurrió un error al obtener los ejercicios desde Firebase Firestore
-            exception.printStackTrace()
+            println("Error en la consulta: ${exception.message}")
         }
 }
